2fdb78cf03c9900be29f0fdcecc787a537d6be8c
oğuzhan cengiz <ozancengiz898@gmail.com>
2021-05-06 23:08:14 +0300

    06.05.2021

--- MvcProject.Mvc/Controllers/CategoryController.cs
+++ MvcProject.Mvc/Controllers/CategoryController.cs
@@ -3,15 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MvcProject.Business.Concrete;
+using MvcProject.DataAccess.Concrete;
 
 namespace MvcProject.Mvc.Controllers
 {
     public class CategoryController : Controller
     {
         // GET: Category
+        CategoryManager categoryManager=new CategoryManager(new EfCategoryDal());
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult GetCategoryList()
+        {
+            var categoryValues = categoryManager.GetAll();
+            return View(categoryValues);
         }
     }
 }
