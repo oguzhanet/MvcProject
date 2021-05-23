9c7fab6c2bf80ea2193cfd7e7b695b69a57561a3
oğuzhan cengiz <ozancengiz898@gmail.com>
2021-05-23 20:41:46 +0300

    23.05.2021

---
+++ MvcProject.DataAccess/Migrations/202105231653165_mig_writer_add_status.cs
@@ -0,0 +1,18 @@
+﻿namespace MvcProject.DataAccess.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class mig_writer_add_status : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Writers", "WriterStatus", c => c.Boolean(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Writers", "WriterStatus");
+        }
+    }
+}
--- MvcProject.Entities/Concrete/Writer.cs
+++ MvcProject.Entities/Concrete/Writer.cs
@@ -20,13 +20,14 @@ namespace MvcProject.Entities.Concrete
         public string WriterAbout { get; set; }
         [StringLength(30)]
         public string WriterTitle { get; set; }
-        [StringLength(100)]
+        [StringLength(250)]
         public string WriterImage { get; set; }
         [StringLength(200)]
         public string WriterMail { get; set; }
         [StringLength(200)]
         public string WriterPassword { get; set; }
-
+        public bool WriterStatus { get; set; }
+        
         public ICollection<Heading> Headings { get; set; }
         public ICollection<Content> Contents { get; set; }
     }
