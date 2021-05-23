1357f5fea9c44322a7723a8ecac543d91d7a95c3
oğuzhan cengiz <ozancengiz898@gmail.com>
2021-05-23 21:15:53 +0300

    23.05.2021

---
+++ MvcProject.Business/Abstract/IContentService.cs
@@ -0,0 +1,19 @@
+﻿using MvcProject.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MvcProject.Business.Abstract
+{
+    public interface IContentService
+    {
+        List<Content> GetAll();
+        List<Content> GetAllById(int id);
+        Content GetById(int id);
+        void Add(Content content);
+        void Update(Content content);
+        void Delete(Content content);
+    }
+}
---
+++ MvcProject.Business/Concrete/ContentManager.cs
@@ -0,0 +1,51 @@
+﻿using MvcProject.Business.Abstract;
+using MvcProject.DataAccess.Abstract;
+using MvcProject.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MvcProject.Business.Concrete
+{
+    public class ContentManager : IContentService
+    {
+        private IContentDal _contentDal;
+
+        public ContentManager(IContentDal contentDal)
+        {
+            _contentDal = contentDal;
+        }
+
+        public List<Content> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Content> GetAllById(int id)
+        {
+            return _contentDal.GetAll(x => x.HeadingId == id);
+        }
+
+        public Content GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Add(Content content)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Content content)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Content content)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
