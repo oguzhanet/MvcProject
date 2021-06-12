abbddb9f10e3382b6a6e70b544de2c435a947049
oğuzhan cengiz <ozancengiz898@gmail.com>
2021-06-12 21:46:28 +0300

    12.06.2021

--- MvcProject.Mvc/Controllers/WriterPanelMessageController.cs
+++ MvcProject.Mvc/Controllers/WriterPanelMessageController.cs
@@ -1,6 +1,8 @@
-﻿using MvcProject.Business.Concrete;
+﻿using FluentValidation.Results;
+using MvcProject.Business.Concrete;
 using MvcProject.Business.ValidationRules.FluentValidation;
 using MvcProject.DataAccess.Concrete.EntityFramework;
+using MvcProject.Entities.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -31,6 +33,69 @@ namespace MvcProject.Mvc.Controllers
         public PartialViewResult MessagePartial()
         {
             return PartialView();
+        }
+
+        public ActionResult GetInBoxMessageDetails(int id)
+        {
+            var values = messageManager.GetById(id);
+            return View(values);
+        }
+
+        public ActionResult GetSendBoxMessageDetails(int id)
+        {
+            var values = messageManager.GetById(id);
+            return View(values);
+        }
+
+        [HttpGet]
+        public ActionResult NewMessage()
+        {
+            return View();
+        }
+
+        [HttpPost] //Burası refaktor edilecek şuanlık bu şekide..
+        public ActionResult NewMessage(Message message, string parameter)
+        {
+            ValidationResult results = messageValidator.Validate(message);
+            if (parameter == "send")
+            {
+                if (results.IsValid)
+                {
+                    message.SenderMail = "kadir@gmail.com";
+                    message.IsDraft = false;
+                    message.MessageDate = DateTime.Parse(DateTime.Now.ToShortDateString());
+                    messageManager.Add(message);
+                    return RedirectToAction("Sendbox");
+                }
+                else
+                {
+                    foreach (var item in results.Errors)
+                    {
+                        ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                    }
+                }
+            }
+
+            else if (parameter == "draft")
+            {
+                if (results.IsValid)
+                {
+                    message.SenderMail = "kadir@gmail.com";
+                    message.IsDraft = true;
+                    message.MessageDate = DateTime.Parse(DateTime.Now.ToShortDateString());
+                    messageManager.Add(message);
+                    return RedirectToAction("Draft");
+                }
+                else
+                {
+                    foreach (var item in results.Errors)
+                    {
+                        ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                    }
+                }
+            }
+
+            return View();
         }
     }
 }
