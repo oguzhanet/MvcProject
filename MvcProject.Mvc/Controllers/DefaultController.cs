d762fce9074195e79338baacfc928059914018ff
oğuzhan cengiz <ozancengiz898@gmail.com>
2021-06-18 21:42:33 +0300

    18.06.2021

---
+++ MvcProject.Mvc/Controllers/DefaultController.cs
@@ -0,0 +1,28 @@
+﻿using MvcProject.Business.Concrete;
+using MvcProject.DataAccess.Concrete.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProject.Mvc.Controllers
+{
+    [AllowAnonymous]
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        HeadingManager headingManager = new HeadingManager(new EfHeadingDal());
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Headings()
+        {
+            var headingList = headingManager.GetAll();
+            return View(headingList);
+        }
+    }
+}
