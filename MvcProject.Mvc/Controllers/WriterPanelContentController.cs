4c0c0dbb7364e0c1fdeed62db31fa4f4bdb8c7a6
oğuzhan cengiz <ozancengiz898@gmail.com>
2021-06-19 14:29:29 +0300

    19.06.2021

--- MvcProject.Mvc/Controllers/WriterPanelContentController.cs
+++ MvcProject.Mvc/Controllers/WriterPanelContentController.cs
@@ -2,6 +2,7 @@
 using MvcProject.Business.Concrete;
 using MvcProject.DataAccess.Concrete;
 using MvcProject.DataAccess.Concrete.EntityFramework;
+using MvcProject.Entities.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,6 +15,7 @@ namespace MvcProject.Mvc.Controllers
     {
         // GET: WriterPanelContent
         //ContentManager contentManager = new ContentManager(new EfContentDal());
+        Context context = new Context();
         private IContentService _contentService;
 
         public WriterPanelContentController(IContentService contentService)
@@ -23,12 +25,30 @@ namespace MvcProject.Mvc.Controllers
 
         public ActionResult MyContent(string parameter)
         {
-            Context context = new Context();
+            
             parameter = (string)Session["WriterMail"];
             var writerIdInfo = context.Writers.Where(x => x.WriterMail == parameter).Select(z => z.WriterId).FirstOrDefault();
             
             var contentValues = _contentService.GetAllByWriter(writerIdInfo);
             return View(contentValues);
+        }
+
+        [HttpGet]
+        public ActionResult AddContent()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddContent(Content content)
+        {
+            string mail = (string)Session["WriterMail"];
+            var writerIdInfo = context.Writers.Where(x => x.WriterMail == mail).Select(z => z.WriterId).FirstOrDefault();
+            content.ContentDate =DateTime.Parse(DateTime.Now.ToShortDateString());
+            content.WriterId = writerIdInfo;
+            content.ContentStatus = true;
+            _contentService.Add(content);
+            return RedirectToAction("MyContent");
         }
     }
 }
