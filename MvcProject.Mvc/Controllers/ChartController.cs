55a74e7e5c503740f9ea7b85c1f725eb2bf17545
oğuzhan cengiz <ozancengiz898@gmail.com>
2021-07-02 22:47:11 +0300

    02.07.2021

---
+++ MvcProject.Mvc/Controllers/ChartController.cs
@@ -0,0 +1,29 @@
+﻿using MvcProject.Business.Concrete;
+using MvcProject.DataAccess.Concrete;
+using MvcProject.DataAccess.Concrete.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProject.Mvc.Controllers
+{
+    public class ChartController : Controller
+    {
+        CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+        Context context = new Context();
+        // GET: Chart
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult CategoryChart()
+        {
+            var result = context.Categories.Count();
+            ViewBag.r = result;
+            return Json(categoryManager.GetAll(),JsonRequestBehavior.AllowGet);
+        }
+    }
+}
