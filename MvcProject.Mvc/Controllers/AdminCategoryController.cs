2650a365faad91db506a5436d42cf24306db045b
oğuzhan cengiz <ozancengiz898@gmail.com>
2021-07-05 19:52:41 +0300

    05.07.2021

--- MvcProject.Mvc/Controllers/AdminCategoryController.cs
+++ MvcProject.Mvc/Controllers/AdminCategoryController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Web;
 using System.Web.Mvc;
 using FluentValidation.Results;
@@ -44,7 +45,8 @@ namespace MvcProject.Mvc.Controllers
             {
                 category.CategoryStatus = true;
                 categoryManager.Add(category);
-                return View();
+                Thread.Sleep(1500);
+                return RedirectToAction("Index");
             }
             else
             {
