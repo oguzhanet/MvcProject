776a339c11ccb666a3c73423b73c8ef4c49237e2
oğuzhan cengiz <ozancengiz898@gmail.com>
2021-07-07 16:20:31 +0300

    07.07.2021

--- MvcProject.Mvc/Controllers/TalentCardController.cs
+++ MvcProject.Mvc/Controllers/TalentCardController.cs
@@ -13,40 +13,17 @@ namespace MvcProject.Mvc.Controllers
     public class TalentCardController : Controller
     {
         // GET: TalentCard
-        //TalentCardManager talentCardManager = new TalentCardManager(new EfTalentCardDal());
-        private ITalentCardService _talentCardService;
+        TalentCardManager talentCardManager = new TalentCardManager(new EfTalentCardDal());
+        //private ITalentCardService _talentCardService;
 
-        public TalentCardController(ITalentCardService talentCardService)
-        {
-            _talentCardService = talentCardService;
-        }
+        //public TalentCardController(ITalentCardService talentCardService)
+        //{
+        //    _talentCardService = talentCardService;
+        //}
 
         public ActionResult Index()
         {
-            Context context = new Context();
-            var cardValues = _talentCardService.GetAll();
-
-            var result = context.TalentCards.Sum(x => x.SkillPoint) * 250 / 100;
-            ViewBag.result = result;
-            var result2 = context.TalentCards.Sum(x => x.SkillPoint2) * 250 / 100;
-            ViewBag.result2 = result2;
-            var result3 = context.TalentCards.Sum(x => x.SkillPoint3) * 250 / 100;
-            ViewBag.result3 = result3;
-            var result4 = context.TalentCards.Sum(x => x.SkillPoint4) * 250 / 100;
-            ViewBag.result4 = result4;
-            var result5 = context.TalentCards.Sum(x => x.SkillPoint5) * 250 / 100;
-            ViewBag.result5 = result5;
-            var result6 = context.TalentCards.Sum(x => x.SkillPoint6) * 250 / 100;
-            ViewBag.result6 = result6;
-            var result7 = context.TalentCards.Sum(x => x.SkillPoint7) * 250 / 100;
-            ViewBag.result7 = result7;
-            var result8 = context.TalentCards.Sum(x => x.SkillPoint8) * 250 / 100;
-            ViewBag.result8 = result8;
-            var result9 = context.TalentCards.Sum(x => x.SkillPoint9) * 250 / 100;
-            ViewBag.result9 = result9;
-            var result10 = context.TalentCards.Sum(x => x.SkillPoint10) * 250 / 100;
-            ViewBag.result10 = result10;
-
+            var cardValues = talentCardManager.GetAll();
             return View(cardValues);
         }
 
