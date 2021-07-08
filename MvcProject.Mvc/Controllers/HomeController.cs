1e552b1b7a3e73c8c0d3257099d4403d485b818a
oğuzhan cengiz <ozancengiz898@gmail.com>
2021-07-08 14:24:36 +0300

    08.07.2021

--- MvcProject.Mvc/Controllers/HomeController.cs
+++ MvcProject.Mvc/Controllers/HomeController.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using MvcProject.Business.Concrete;
+using MvcProject.DataAccess.Concrete;
+using MvcProject.DataAccess.Concrete.EntityFramework;
+using MvcProject.Entities.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -28,9 +32,33 @@ namespace MvcProject.Mvc.Controllers
         }
 
         [AllowAnonymous]
+        [HttpGet]
         public ActionResult HomePage()
         {
+            Context context = new Context();
+            var result = context.Headings.Count().ToString();
+            ViewBag.result = result;
+
+            var result1 = context.Writers.Count().ToString();
+            ViewBag.result1 = result1;
+
+            var result3 = context.Messages.Count().ToString();
+            ViewBag.result3 = result3;
+
+            var result4 = context.Contents.Count().ToString();
+            ViewBag.result4 = result4;
+
             return View();
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public ActionResult ContactAdd(Contact contact)
+        {
+            ContactManager contactManager = new ContactManager(new EfContactDal());
+            contact.ContactDate = DateTime.Parse(DateTime.Now.ToString());
+            contactManager.Add(contact);
+            return RedirectToAction("HomePage");
         }
     }
 }
